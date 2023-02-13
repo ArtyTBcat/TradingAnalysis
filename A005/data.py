cc76325cb0ed88c10b6d6e0d0586c1975c9dbc79
Nontakorn Bunluesriruang <77570219+ArtyTBcat@users.noreply.github.com>
2023-02-13 23:28:45 +0700

    Add files via upload

---
+++ A005/data.py
@@ -0,0 +1,72 @@
+import yfinance as yf
+import numpy as np
+import pandas as pd
+from A004_copy.A004_01 import main as A004Main
+
+currency = ['BTC', 'BNB', 'ETH', 'ADA', 'XRP',
+            'SOL', 'TRX', 'VET', 'FTM', 'DOT', 'LTC', 'ATOM']
+
+
+def APIdata(tickerNum: int):  # DataSet historical[1],
+    historicalClose = list(yf.download(
+        currency[tickerNum] + '-USD', period='max', interval='1d', progress=False)['Close'])
+    historicalClose.reverse()
+    return historicalClose
+
+
+class models:
+    def percentChange(tickerNum: int):
+        listChg = []
+        abs_listChg = []
+        historical = APIdata(tickerNum)
+        for count in range(len(historical)):
+            try:
+                percentChg = (
+                    (historical[count] - historical[count+1]) / historical[count+1]) * 100
+                listChg.append(percentChg)
+                abs_listChg.append(percentChg)
+            except:
+                pass
+        return listChg, abs_listChg
+
+    def volatility(tickerNum: int):
+        vol = np.average(models.percentChange(tickerNum)[1])
+        return vol
+
+
+class collections:
+    def volatility():  # study if volatility impact the predicted values
+        var = {'predicted': [], 'volatility': [],
+               'accuracy': [], 'nValues': []}
+        try:
+            df = pd.read_csv('data.csv')
+            dataRead = df.to_dict()
+        except:
+            df = pd.DataFrame(var)
+            df.to_csv('data.csv')
+
+        for x in range(len(currency)):
+            PREDICTED = A004Main(x)['predictedPrice']
+            var['predicted'].append(PREDICTED)
+            var['volatility'].append(models.volatility(x))
+
+            accuracy = (
+                (APIdata(x)[0] - dataRead['predicted'][x]) / dataRead['predicted'][x]) * 100
+            xAccuracy = (
+                ((dataRead['accuracy'][x] * dataRead['nValues'][x]) + accuracy) / dataRead['nValues'][x])
+            var['accuracy'].append(xAccuracy)
+
+            try:
+                var['nValues'].append(dataRead['nValues'][x] + 1)
+            except:
+                var['nValues'].append(1)
+        df = pd.DataFrame(var)
+        df.to_csv('data.csv')
+
+        print(pd.read_csv('data.csv'))
+
+
+for x in range(len(currency)):
+    print(currency[x], models.volatility(x))
+
+# collections.volatility()
